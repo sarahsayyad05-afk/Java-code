885cab60b7e5e805a3ebec84718d058f77a4b596
Sarah <sarahsayyad05@gmail.com>
2026-06-16 21:54:47 +0530

    ArrayList example

--- ArrayLists/Arraylistdemo.java
+++ ArrayLists/Arraylistdemo.java
@@ -4,7 +4,7 @@ public class Arraylistdemo {
     public static void main(String[] args) {
         ArrayList<String> fruits=new ArrayList<>();
         fruits.add("Apple");
-        fruits.add("Banana");
+        fruits.add("Banana"); 
         fruits.add("Cherry");
         fruits.add("Date");
         
