961b08b694841b81fb5c0f833b20edebbf011143
Sarah <sarahsayyad05@gmail.com>
2026-09-14 21:09:23 +0530

    Mathpack Package

---
+++ Packages/mathpack/area.java
@@ -0,0 +1,6 @@
+package mathpack;
+public class area extends calculator{
+public int area(int a){
+    return a*a;
+ }
+}
---
+++ Packages/mathpack/calculator.java
@@ -0,0 +1,16 @@
+package mathpack;
+public class calculator{
+public int add(int a, int b){
+    return a+b;
+}
+public int sub(int a,int b){
+   return a-b;
+}
+public double mul(double a, double b){
+    return a*b;
+}
+public double div(double a, double b){
+    return a/b;
+}
+
+}
